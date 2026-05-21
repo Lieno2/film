00fd8f36500005a43ec46ee05549be7a7c81f177
Lieno2 <morganix18@gmail.com>
2026-05-21 11:45:00 +0200

    Added form

---
+++ script.js
@@ -0,0 +1,26 @@
+ /* 
+Data structure for a film:
+ {
+    title,
+    duration,
+    watch_date,
+    cinema,
+}
+*/
+
+
+// TODO: Implement the following functions
+function addFilm() {
+    
+}
+function removeFilm() {}
+function clearFilms() {}
+
+function displayFilms() {}
+function getTotalWatchTime() {}
+function getFilmsWatchedInCinema() {}
+
+function saveFilmsToLocalStorage() {}
+function loadFilmsFromLocalStorage() {}
+
+function getAiRecommendedFilms() {}
