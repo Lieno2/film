675b6a20bc4f79bffc368305e82ec0f8152ede19
Lieno2 <morganix18@gmail.com>
2026-05-21 11:20:18 +0200

    Added guidelines for the project

---
+++ script.js
@@ -0,0 +1,24 @@
+ /* 
+Data structure for a film:
+ {
+    title,
+    duration,
+    watch_date,
+    cinema,
+}
+*/
+
+
+// TODO: Implement the following functions
+function addFilm() {}
+function removeFilm() {}
+function clearFilms() {}
+
+function displayFilms() {}
+function getTotalWatchTime() {}
+function getFilmsWatchedInCinema() {}
+
+function saveFilmsToLocalStorage() {}
+function loadFilmsFromLocalStorage() {}
+
+function getAiRecommendedFilms() {}
