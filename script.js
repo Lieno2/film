3f2b1bb3a426e98974326ca45f459ff0b5103542
nizar <nizarlol48@gmail.com>
2026-05-21 12:38:48 +0200

    Implemented the first 3 functions

--- script.js
+++ script.js
@@ -9,10 +9,49 @@ Data structure for a film:
 */
 
 
+let nome = document.getElementById("myName");
+let durata = document.getElementById("myDuration");
+let data = document.getElementById("myDate");
+let check = document.getElementById("myCheck");
+
+let elencoFilm = [];
+
 // TODO: Implement the following functions
-function addFilm() {}
-function removeFilm() {}
-function clearFilms() {}
+function addFilm() {
+    alert("hi");
+
+    let titleVal = nome.value;
+    let durationVal = durata.value;
+    let watch_dateVal = data.value;
+    let cinemaVal = check.value;
+
+    let film{
+        title: titleVal,
+        duration: durationVal,
+        watch_date: watch_dateVal,
+        cinema: cinemaVal
+    }
+
+    for(let i = 0; i < elencoFilm.length; i++){
+        if(titleVal.toLowerCase() == elencoFilm[i].title){
+            alert("film giè inserito");
+        } else{
+            elencoFilm.push(film);
+        }
+    }
+}
+
+function removeFilm() {
+    for(let i = 0; i < elencoFilm.length; i++){
+        if(titleVal.toLowerCase() == elencoFilm[i].title){
+            elencoFilm.pop(i);
+        }
+    }
+}
+
+function clearFilms() {
+    elencoFilm = [];
+}
 
 function displayFilms() {}
 function getTotalWatchTime() {}
